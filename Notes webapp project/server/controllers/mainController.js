ba419f9385119b1e246e531ccbe214fd9e7530c4
tanishq2507 <smartlohia@gmail.com>
2023-10-03 06:50:00 +0530

    first commit

---
+++ Notes webapp project/server/controllers/mainController.js
@@ -0,0 +1,30 @@
+/**
+ * GET /
+ *  Homepage
+ */
+
+exports.homepage = async(req, res)=>{
+    const locals = {
+        title: "NodeJs Notes",
+        description: "Free NodeJS Notes app."
+    }
+
+    res.render('index',{
+        locals,
+        layout:'../views/layouts/front-page'
+    });
+}
+
+/**
+ * GET /
+ *  About
+ */
+
+exports.about = async(req, res)=>{
+    const locals = {
+        title: "About - NodeJs Notes",
+        description: "Free NodeJS Notes app."
+    }
+
+    res.render('about',locals);
+}
